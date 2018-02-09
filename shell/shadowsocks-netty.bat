85f5c06d56e8fda4df643640b86d415facd58853
LMoon <jjay033033@gmail.com>
2018-02-09 15:45:33 +0800

    启动时增加监控参数

--- shell/shadowsocks-netty.bat
+++ shell/shadowsocks-netty.bat
@@ -1,3 +1,3 @@
 @echo off
-java -jar shadowsocks-netty-1.0.0.jar
+java -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=8011 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -jar shadowsocks-netty-1.0.0.jar
 pause
